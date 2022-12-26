ecd245308daa86e61f97b81fa87a32dc030156d9
TinChung41 <tinchung21@gmail.com>
2022-12-26 21:47:21 +0700

    runability check 2

--- PlantsVsZombies/PlantsVsZombies/src/GatlingPea.java
+++
@@ -1,6 +0,0 @@
-public class GatlingPea extends Pea{
-    public GatlingPea(GamePanel parent,int lane,int startX){
-    	super(parent,lane,startX);
-    	damage = 250;
-    }
-}
--- PlantsVsZombies/PlantsVsZombies/src/WallNut.java
+++
@@ -1,8 +0,0 @@
-
-public class WallNut extends Defender {
-
-	public WallNut(GamePanel parent, int x, int y) {
-		super(parent, x, y);
-		ExtraHealth(1800);
-	}
-}
